09e76ec9e4b11c4d373046ff2062ac3a906f2e5b
Kelton Karboviak <kelton.karboviak@gmail.com>
2018-05-19 13:37:17 -0500

    Modified setup.py to include CHANGELOG.md in long_description

--- setup.py
+++ setup.py
@@ -9,13 +9,16 @@ from setuptools import setup
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.md'), 'r', encoding='utf-8') as fp:
-    long_description = fp.read()
+    readme = fp.read()
+
+with open(path.join(here, 'CHANGELOG.md'), 'r', encoding='utf-8') as fp:
+    change_log = fp.read()
 
 setup(
     name='slack_log_utils',
     version='0.1.0',
     description='A Python logging handler for Slack integration',
-    long_description=long_description,
+    long_description=readme + '\n\n' + change_log,
     long_description_content_type='text/markdown',
     classifiers=[
         'Development Status :: 4 - Beta',
